ad73ec608ad979bb5a416861ca12abdf5aca3fba
alunoinfo <alunoinfo@LAB05M03.ifsul-sapucaia.local>
2014-08-13 16:35:28 -0300

    Teste commit

--- src/com/example/handcoach/TelaNav.java
+++ src/com/example/handcoach/TelaNav.java
@@ -10,6 +10,7 @@ public class TelaNav extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.tela_nav);
+		//teste commit
 	}
 
 	@Override
