10262f1944e548c2c14d6eb0385a0402a4c6089b
teste <teste@teste.com>
2014-09-04 12:07:14 -0300

    - Prenúncio de uma listView bonitinha

---
+++ src/com/example/handcoach/LazyAdapter.java
@@ -0,0 +1,35 @@
+package com.example.handcoach;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+public class LazyAdapter extends BaseAdapter {
+
+	@Override
+	public int getCount() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Object getItem(int arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public long getItemId(int arg0) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public View getView(int arg0, View arg1, ViewGroup arg2) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	//http://www.androidhive.info/2012/02/android-custom-listview-with-image-and-text/
+
+}
